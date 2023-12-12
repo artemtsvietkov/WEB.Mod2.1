ef8adfe29b482b04d9eba9a041c7d19b7e09dd57
artemtsvietkov <143085970+artemtsvietkov@users.noreply.github.com>
2023-12-12 12:41:32 +0100

    Web. Mod2.1

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+function doSomething(){
+    document.getElementById("button").innerHTML = "goodbye"
+    document.getElementById("button").style.backgroundColor = "yellow"
+}
